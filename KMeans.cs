48fc793878c2ba69fd9dec1c3566c732dfb5cb6e
SmokyOoOwizard <smokyOoOwizard@yandex.ru>
2019-03-17 14:02:54 +0200

    Initial commit

---
+++ KMeans.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KMeans
+{
+    public class KMeans
+    {
+        public VectorN[] Data;
+        public VectorN[] clusters;
+        public int ClustersCount;
+        public int Seed;
+
+        public KMeans(VectorN[] data, int clustersCount, int Seed)
+        {
+            Data = data;
+            ClustersCount = clustersCount;
+            clusters = new VectorN[ClustersCount];
+            this.Seed = Seed;
+        }
+        void Clustering()
+        {
+            Dictionary<VectorN, List<VectorN>> centerAssignments = GetCenterAssignments(Data, PickRandomCenters(ClustersCount, Data));
+
+            List<VectorN> oldCenters = null;
+            while (true)
+            {
+                List<VectorN> newCenters = GetNewCenters(centerAssignments);
+
+                if (CentersEqual(newCenters, oldCenters))
+                {
+                    break;
+                }
+
+                centerAssignments = GetCenterAssignments(Data, newCenters);
+
+                oldCenters = newCenters;
+            }
+        }
+        private bool CentersEqual(List<VectorN> newCenters, List<VectorN> oldCenters)
+        {
+            if (newCenters == null || oldCenters == null)
+            {
+                return false;
+            }
+
+            foreach (VectorN newCenter in newCenters)
+            {
+                bool found = false;
+                foreach (VectorN oldCenter in oldCenters)
+                {
+                    if (newCenter.Equals(oldCenter))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private List<VectorN> GetNewCenters(Dictionary<VectorN, List<VectorN>> centerAssignments)
+        {
+            VectorN total = new VectorN();
+
+            var newCenters = new List<VectorN>();
+
+            foreach (VectorN center in centerAssignments.Keys)
+            {
+                total = new VectorN();
+
+                foreach (VectorN point in centerAssignments[center])
+                {
+                    total += point;
+                }
+
+                VectorN average = total / centerAssignments[center].Count;
+
+                newCenters.Add(average);
+            }
+            return newCenters;
+        }
+        private Dictionary<VectorN, List<VectorN>> GetCenterAssignments(VectorN[] points, List<VectorN> centers)
+        {
+            var centerAssignments = new Dictionary<VectorN, List<VectorN>>();
+
+            foreach (VectorN point in centers)
+            {
+                centerAssignments.Add(point, new List<VectorN>());
+            }
+
+
+            foreach (VectorN point in points)
+            {
+                VectorN closestCenter = new VectorN();
+                double closestCenterDistance = double.MaxValue;
+
+                foreach (VectorN centerPoint in centers)
+                {
+
+                    float distance = VectorN.Distance(point, centerPoint);
+
+                    if (distance < closestCenterDistance)
+                    {
+                        closestCenterDistance = distance;
+                        closestCenter = centerPoint;
+                    }
+                }
+
+                lock (centerAssignments)
+                {
+                    centerAssignments[closestCenter].Add(point);
+                }
+            }
+
+            return centerAssignments;
+        }
+        private List<VectorN> PickRandomCenters(int clusters, VectorN[] points)
+        {
+            System.Random r = new System.Random(Seed);
+            var randomCenters = new List<VectorN>();
+            int pickedPointCount = 0;
+            while (pickedPointCount < clusters)
+            {
+                var point = points[r.Next(0, points.Length - 1)];
+                if (!randomCenters.Contains(point))
+                {
+                    randomCenters.Add(point);
+                    pickedPointCount++;
+                }
+            }
+            return randomCenters;
+        }
+    }
+}
---
+++ VectorN.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KMeans
+{
+    public class VectorN
+    {
+        float[] Axis = new float[2];
+        public int Dimension
+        {
+            get
+            {
+                if (Axis == null)
+                {
+                    return 0;
+                }
+                return Axis.Length;
+            }
+        }
+
+        public VectorN(int dimension)
+        {
+            dimension = dimension > 0 ? dimension : 2;
+            Axis = new float[dimension];
+        }
+        public VectorN(params float[] axis)
+        {
+            if (axis != null && axis.Length > 0)
+            {
+                Axis = new float[axis.Length];
+                Array.Copy(axis, Axis, Dimension);
+            }
+            else
+            {
+                Axis = new float[2];
+            }
+        }
+        public VectorN(int dimension, float[] axis)
+        {
+            dimension = dimension > 0 ? dimension : 2;
+            Axis = new float[dimension];
+            if (axis != null)
+            {
+                Array.Copy(axis, Axis, Dimension);
+            }
+        }
+        public VectorN(VectorN vector)
+        {
+            Axis = new float[vector.Dimension];
+            if (vector.Axis != null)
+            {
+                Array.Copy(vector.Axis, Axis, Dimension);
+            }
+
+        }
+
+
+        public static float Distance(VectorN a, VectorN b)
+        {
+            return (float)Math.Sqrt(DistanceSqrt(a, b));
+        }
+        public static float DistanceSqrt(VectorN a, VectorN b)
+        {
+            float r = 0;
+            a -= b;
+            foreach (var item in a.Axis)
+            {
+                r += (float)Math.Pow(item, 2f);
+            }
+            return r;
+        }
+
+        public float this[int key]
+        {
+            get
+            {
+                return Axis[key];
+            }
+            set
+            {
+                Axis[key] = value;
+            }
+        }
+        public static VectorN operator +(VectorN a, VectorN b)
+        {
+
+            a.Axis = a.Axis == null ? new float[2] : a.Axis;
+            b.Axis = b.Axis == null ? new float[2] : b.Axis;
+            /*
+            int m = a.N >= b.N ? a.N : b.N;
+
+            float[] q = new float[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                q[i] = (i < a.N ? a[i] : 0) + (i < b.N ? b[i] : 0);
+            }
+            */
+            return new VectorN(a.Axis[0] + b.Axis[0], a.Axis[1] + b.Axis[1]);
+        }
+        public static VectorN operator -(VectorN a, VectorN b)
+        {
+
+            a.Axis = a.Axis == null ? new float[2] : a.Axis;
+            b.Axis = b.Axis == null ? new float[2] : b.Axis;
+            /*
+            int m = a.N >= b.N ? a.N : b.N;
+
+            float[] q = new float[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                q[i] = (i < a.N ? a[i] : 0) - (i < b.N ? b[i] : 0);
+            }
+            */
+            return new VectorN(a.Axis[0] - b.Axis[0], a.Axis[1] - b.Axis[1]);
+        }
+        public static VectorN operator /(VectorN a, int b)
+        {
+            /*
+            a.Axis = a.Axis == null ? new float[1] : a.Axis;
+            for (int i = 0; i < a.N; i++)
+            {
+                a[i] /= b;
+            }
+            
+            return a;
+            */
+            return new VectorN(a.Axis[0] / b, a.Axis[1] / b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+            VectorN n = (VectorN)obj;
+            if (obj.GetType() != typeof(VectorN))
+            {
+                return false;
+            }
+            if (Dimension == n.Dimension)
+            {
+                for (int i = 0; i < Dimension; i++)
+                {
+                    if (Axis[i] != n[i])
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            int i = 0;
+            i += this.Dimension;
+            if (i > 0)
+            {
+                i *= (int)this[0];
+            }
+            return i;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("(");
+            foreach (var item in Axis)
+            {
+                sb.Append(item + ", ");
+            }
+            sb.Remove(sb.Length - 2, 2);
+            sb.Append(")");
+            return sb.ToString();
+        }
+    }
+}
